10ccec5c2ac6171fd613b85c798e061cb4067d63
Liran Tal <liran.tal@gmail.com>
2023-08-16 16:29:12 +0300

    fix: package name changed from a suffix of -plugin to -integration
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- src/pages/api/trigger.js
+++ src/pages/api/trigger.js
@@ -1,4 +1,4 @@
-import { createAstroRoute } from "triggerdev-astro-plugin";
+import { createAstroRoute } from "triggerdev-astro-integration";
 import { client } from "../../../trigger.js";
 
 export const post = createAstroRoute(client);
